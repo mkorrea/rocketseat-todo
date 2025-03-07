4920e799f6c7a3eaadb190487af3f092390648a4
mkorrea <mathscorrea@gmail.com>
2025-03-07 15:50:12 -0300

    feat: add borders to mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="max-w-[738px] w-full z-10 -mt-7 flex flex-col gap-y-16">
+      <div className="max-w-[738px] w-full z-10 -mt-7 flex flex-col gap-y-16 px-6 md:px-0">
         <form action={addTask} className="flex gap-x-2">
           <Input name="task"/> 
           <Button type="submit" />
@@ -91,7 +91,7 @@ export default function Home() {
                   />
                 )
               })}
-              {completedTasks.length && 
+              {completedTasks.length !== 0 && 
                 <div className="h-px bg-gradient-to-r from-gray-600 via-gray-400 to-gray-600" />
               }
               {completedTasks.map(item => {
